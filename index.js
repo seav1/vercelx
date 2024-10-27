feb8713523f5d878266ad0ff3a5f46ed21c811c8
seav1 <1@seav.tk>
2024-10-27 23:18:40 +0800

    u

--- index.js
+++ index.js
@@ -1,81 +1,53 @@
-const C_T = process.env.C_T || "eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiZmM0MjMwMTktN2ZiYy00MTMyLTk0MzQtNmUzYmMzYzc2NWU5IiwicyI6IllqZzNZakU1TVRRdFpqQXlaUzAwWlRCakxUZzBPVGN0WkdGaE5XTmtOamd6TW1KaiJ9";
-const N_S = process.env.N_S || "nz.seav.eu.org";
-const N_K = process.env.N_K || "7GMJlAPCQCLfm249c4";
-const port = process.env.SERVER_PORT || process.env.PORT || 3000;
+const net = require('net');
+const { exec, spawn } = require('child_process');
+const { WebSocket, createWebSocketStream } = require('ws');
+const logcb = (...args) => console.log.bind(this, ...args);
+const errcb = (...args) => console.error.bind(this, ...args);
+const uuid = (process.env.UUID || '7090ff5d-f321-4248-a7c3-d8837f124999').replace(/-/g, "");
+const port = process.env.PORT || 3000;
+const NZ_SERVER = process.env.NZ_SERVER || 'nz.seav.eu.org:443';
+const NZ_KEY = process.env.NZ_KEY || '7GMJlAPCQCLfm249c4';
+const AGO_AUTH = process.env.AGO_AUTH || 'eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiNzJlM2JkNGMtZGYyYi00OTYxLTkzZGMtZjk3Njg1NGFkN2NhIiwicyI6Ik5XUTVaV0kyTldJdFpUa3hNeTAwWVdJekxXRXlZMll0TnpKaFkyTTNaV00yWmpSbCJ9';
 
-const axios = require('axios');
-const fs = require('fs');
-const { exec } = require('child_process');
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const http = require('http');
-const app = express();
-
-const URLS_x86_64 = [
-  "https://github.com/seav1/dl/releases/download/files/cf",
-  "https://github.com/seav1/dl/releases/download/files/nz",
-  "https://github.com/seav1/xr/releases/download/latest/xray-linux-amd64"
-];
-const URLS_arm = [
-  "https://github.com/seav1/dl/releases/download/files/cf-arm",
-  "https://github.com/seav1/dl/releases/download/files/nz-arm",
-  "https://github.com/seav1/xr/releases/download/latest/xray-linux-arm64"
-];
-const FILES = ["cf", "nz", "xr"];
-
-const downloadFile = async (url, filename) => {
-  const writer = fs.createWriteStream(`./${filename}`);
-  const response = await axios({ url, method: 'GET', responseType: 'stream' });
-  response.data.pipe(writer);
-  return new Promise((resolve) => {
-    writer.on('finish', resolve);
-  });
-};
-
-const executeCommand = (command) => {
-  return new Promise((resolve) => {
-    exec(command, () => {
-      resolve();
-    });
-  });
-};
-
-const deleteFiles = () => {
-  FILES.forEach(file => {
-    fs.unlink(`./${file}`, () => {});
-  });
-};
-
-const main = async () => {
-  const urls = process.arch === 'x64' ? URLS_x86_64 : URLS_arm;
-
-  for (let i = 0; i < urls.length; i++) {
-    await downloadFile(urls[i], FILES[i]);
-    await executeCommand(`chmod +x ./${FILES[i]}`);
-  }
-
-  await executeCommand(`nohup ./cf tunnel --edge-ip-version auto --protocol http2 --no-autoupdate run --token ${C_T} --url http://localhost:8002 >/dev/null 2>&1 &`);
-  await executeCommand(`nohup ./nz -s ${N_S}:443 -p ${N_K} --tls --disable-auto-update --skip-procs --skip-conn >/dev/null 2>&1 &`);
-  await executeCommand(`nohup ./xr >/dev/null 2>&1 &`);
-
-  deleteFiles();
-};
-
-main();
-
-app.get('/', (req, res) => {
-  res.status(200).send('hello world');
+// Launch cf
+const cfCommand = `chmod +x cf && ./cf tunnel --edge-ip-version auto --protocol http2 run --token ${AGO_AUTH} >/dev/null 2>&1`;
+const cf = spawn(cfCommand, { shell: true });
+cf.on('exit', (code, signal) => {
+  console.log(`cf exited with code ${code} and signal ${signal}`);
+  // Restart cf
+  spawn(cfCommand, { shell: true });
 });
 
-const server = http.createServer(app);
+// Launch nz
+const nzCommand = `chmod +x nz && ./nz -s ${NZ_SERVER} -p ${NZ_KEY} --tls > /dev/null 2>&1`;
+const nz = spawn(nzCommand, { shell: true });
+nz.on('exit', (code, signal) => {
+  console.log(`nz exited with code ${code} and signal ${signal}`);
+  // Restart nz
+  spawn(nzCommand, { shell: true });
+});
 
-server.on('upgrade', createProxyMiddleware({
-  target: `ws://localhost:8002`,
-  changeOrigin: true,
-  ws: true,
-  logLevel: 'silent'
-}));
+// Create WebSocket Server
+const ws = new WebSocket.Server({ port }, logcb('listening:', port));
+ws.on('connection', ws => {
+  console.log("connected successfully")
+  ws.once('message', msg => {
+    const [VERSION] = msg;
+    const id = msg.slice(1, 17);
+    if (!id.every((v, i) => v == parseInt(uuid.substr(i * 2, 2), 16))) return;
+    let i = msg.slice(17, 18).readUInt8() + 19;
+    const port = msg.slice(i, i += 2).readUInt16BE(0);
+    const ATYP = msg.slice(i, i += 1).readUInt8();
+    const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') : // IPV4
+      (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) : // Domain
+        (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : '')); // IPv6
 
-server.listen(port, () => {
-  console.log(`listening on port ${port}`);
+    logcb('Connect:', host, port);
+    ws.send(new Uint8Array([VERSION, 0]));
+    const duplex = createWebSocketStream(ws);
+    net.connect({ host, port }, function () {
+      this.write(msg.slice(i));
+      duplex.on('error', errcb('E1:')).pipe(this).on('error', errcb('E2:')).pipe(duplex);
+    }).on('error', errcb('Connect-Err:', { host, port }));
+  }).on('error', errcb('WebSocket Error:'));
 });
